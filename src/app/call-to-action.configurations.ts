fb727e73ae8f2c38285710bc0923898a09a66796
AdamKukiela <kukielaadam4@gmail.com>
2022-04-06 17:06:37 +0200

    Conditional displaying accodring by path on Router

---
+++ src/app/call-to-action.configurations.ts
@@ -0,0 +1,18 @@
+import { CTAConfiguration } from './call-to-action/call-to-action.component';
+
+export const callToAction: { [key: string]: CTAConfiguration } = {
+  assistants: {
+    showForm: true,
+    header: 'Are you interested',
+    description:
+      'Get your own assistant working for you and invest in your growth. Here it is your interest that is the priority',
+    buttonText: 'GET STARTED!',
+  },
+
+  default: {
+    header: 'Are you interested?',
+    description: 'Or maybe you have questions?',
+    buttonText: 'CONTACT US!',
+    showForm: false,
+  },
+};
